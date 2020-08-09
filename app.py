3ac85dfcf8be866d71fb76e4e2a52afb70c02d34
Saimon <56609203+ribeirosaimon@users.noreply.github.com>
2020-08-09 18:23:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_restful import Api
 from resources.carteira import Carteira, Acao
 from resources.usuario import Usuarios, Usuario
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
